9ee072ffe747a4321f198f1e3c117702e086b102
Olek MP <olek.mp23@gmail.com>
2022-07-02 13:52:40 -0500

    Add createCourseHandler - dto - entity

--- 01-03-architectured-gin-healthcheck/internal/platform/server/handler/courses/create.go
+++ 01-03-architectured-gin-healthcheck/internal/platform/server/handler/courses/create.go
@@ -3,11 +3,32 @@ package courses
 import (
 	"net/http"
 
+	mooc "github.com/Sraik25/go-hexagonal_http_api/01-03-architectured-gin-healthcheck/internal"
 	"github.com/gin-gonic/gin"
 )
 
+type createRequest struct {
+	ID       string `json:"id" binding:"required"`
+	Name     string `json:"name" binding:"required"`
+	Duration string `json:"duration" binding:"required"`
+}
+
+// CreateHandler returns an HTTP handler for courses creation
 func CreateHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "everything is ok!")
+		var req createRequest
+		if err := ctx.BindJSON(&req); err != nil {
+			ctx.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		course := mooc.NewCourse(req.ID, req.Name, req.Duration)
+
+		if err := Save(ctx, course); err != nil {
+			ctx.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		ctx.Status(http.StatusCreated)
 	}
 }
